91dc1e767b0dc52023239a55ff09ef80e5b1bf64
Your NameSalvatore <salvatorequagliariello0@gmail.com>
2023-09-24 13:00:16 +0200

    fix style changing on selected chat

--- components/ChatsPreview.vue
+++ components/ChatsPreview.vue
@@ -5,24 +5,26 @@
 
     const nuxt: NuxtApp = useNuxtApp();
     const db = <Firestore>nuxt.$firestore;
-    const auth = <Auth>nuxt.$auth;
+
     const currentUser = useFirebaseUser().value;
     const selectedChat = userConversation();
-
     const chats = ref();
+    const selectedUserId = ref();
+    const selected = "selected-chat";
     
-        if (currentUser) {
-            onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
-                if (doc.exists()) {
-                    const res: DocumentData | undefined = doc.data();
-                    const sortedRes = Object.entries(res).sort((a: any, b: any) => b[1].date - a[1].date);
-                    chats.value = sortedRes;
-                };
-            });
-        };
+    if (currentUser) {
+        onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+            if (doc.exists()) {
+                const res: DocumentData | undefined = doc.data();
+                const sortedRes = Object.entries(res).sort((a: any, b: any) => b[1].date - a[1].date);
+                chats.value = sortedRes;
+            };
+        });
+    };
         
     const changeSelectedChat = (user: DocumentData) => {
         selectedChat.value.user = user;
+        selectedUserId.value = user.uid;
         
         if (currentUser) {
             selectedChat.value.chatId = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid;
@@ -35,56 +37,63 @@
 
 
 <template>
-        <div class="chat-previews">
-            <ul>
-                <li v-for="chat in chats" :key="chat[1].userInfo.uid" @click="changeSelectedChat(chat[1].userInfo)">
-                    <div class="chat-preview__container">
-                        <img :src="chat[1].userInfo.photoURL" />
-                        <div class="chat-preview__details">
-                            <p>{{ chat[1].userInfo.displayName }}</p>
-                            <p>{{ chat[1].lastMessage["text"] }}</p>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-        </div>
+    <ul class="chats-preview">
+        <li 
+            v-for="chat in chats" 
+            :key="chat[1].userInfo.uid" 
+            @click="changeSelectedChat(chat[1].userInfo)" 
+            class="user-preview"
+            :class="selectedUserId === chat[1].userInfo.uid && selected"
+        >
+            <img :src="chat[1].userInfo.photoURL" />
+            <div class="chat-preview__details">
+                <p>{{ chat[1].userInfo.displayName }}</p>
+                <p>{{ chat[1].lastMessage["text"] }}</p>
+            </div>
+        </li>
+    </ul>
 </template>
 
 
-<style scoped>
+<style scoped lang="scss">
+@import '~/assets/css/main.scss';
 
-    .chat-previews {
+    .chats-preview {
+        width: 100%;
+
         display: flex;
         flex-direction: column;
-        width: 100%;
+        gap: 1rem;
+
         margin-top: 1rem;
         padding: 1rem;
         overflow-y: auto;
     }
     
-    .chat-preview__container {
-        box-sizing: border-box;
-        display: flex;
+    .user-preview {
         width: 100%;
+
+        display: flex;
         align-items: center;
+
+        background-color: $alt-secondary-color;
+        color: $text-color;
+        border-radius: 1.5rem;
         padding: 0.5rem 1rem;
-    }
-    
-    li {
-        border-radius: 20px;
-        margin-bottom: 1rem;
-        background-color: white;
+
+        &:hover {
+            background-color: $alt-secondary-color-hover;
+            cursor: pointer;
+        }
+
+        img {
+            width: 50px;
+            border-radius: 100%;
+            margin-right: 1rem;
+        }
     }
 
-    li:hover {
-        background-color: grey;
-        cursor: pointer;
+    .selected-chat {
+        background-color: $alt-secondary-color-hover;
     }
-
-    li img {
-        width: 50px;
-        border-radius: 100%;
-        margin-right: 1rem;
-    }
-    
 </style>
