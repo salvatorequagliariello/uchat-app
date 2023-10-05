49926166d8346917aa9d02aad0cada7914e2d6e9
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-10-05 19:28:18 +0200

    add chat component

--- pages/index.vue
+++ pages/index.vue
@@ -8,18 +8,7 @@
 
 <template>
         <div class="homepage">
-            <div class="app">
-                <div class="chats-container" v-if="userDetails">
-                    <div class="chats-container__header">
-                        <UserHeader :user="userDetails" />
-                        <UsersSearch />
-                    </div>
-                    <ChatsPreview />
-                </div>
-                <div class="chat-view">
-                    <ChatView />
-                </div>
-            </div>
+            <Chat />
         </div>
 </template>
 
@@ -30,50 +19,5 @@
         height: 100vh;
 
         background-color: $alt-secondary-color;
-    }
-    .app {
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%,  -50%);
-
-        width: 900px;
-        height: 600px;
-
-        display: flex;
-        align-items: center;
-        justify-content: center;
-
-        background-color: $primary-color;
-        border-radius: 1rem;
-    }
-
-    .chats-container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-start;
-
-        height: 100%;
-        width: 320px;
-    }
-
-    .chats-container__header {
-        width: 100%;
-        padding: 1.5rem;
-
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-        background-color: $secondary-color;
-
-        border-top-left-radius: 1rem;
-        border-bottom-left-radius: 1rem;
-        border-bottom-right-radius: 1rem;
-    }
-    
-    .chat-view {
-        width: 580px;
-        height: 100%;
     }
 </style>
